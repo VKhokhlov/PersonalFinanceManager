cf9f9bf1bb1f7278135ecc6204cda7bc987cdb03
Vladimir Khokhlov <vladimirkhokhlov@yandex.ru>
2022-10-14 12:12:25 +0700

    Added autosave data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,15 @@
 public class Main {
     public static final int PORT = 8989;
     public static final String CATEGORIES_FILE_NAME = "categories.tsv";
+    public static final String DATA_FILE_NAME = "data.bin";
     public static final String DEFAULT_CATEGORY = "другое";
 
     public static void main(String[] args) {
         try {
             FinanceManager financeManager = new FinanceManager(CATEGORIES_FILE_NAME, DEFAULT_CATEGORY);
-            Server server = new Server(PORT, financeManager);
+            financeManager.loadPurchases(DATA_FILE_NAME);
+
+            Server server = new Server(PORT, DATA_FILE_NAME, financeManager);
             server.start();
         } catch (RuntimeException e) {
             System.out.println("Runtime error: " + e.getMessage());
